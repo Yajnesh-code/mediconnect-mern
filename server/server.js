326493b5608703494ed2bec98bdd3dd707c266c1
Yajnesh-code <yajneshrkulal@gmail.com>
2025-11-20 13:40:36 +0530

    fix: update MongoDB connection to process.env.MONGO_URI

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://127.0.0.1:27017/mediconnect', {
+mongoose.connect(process.env.MONGO_UR, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
